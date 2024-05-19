fc2dca07ba869b90e3cb5b40294f9c0873de943c
johnsone006 <johnsone006@mymail.mssu.edu>
2024-05-18 20:56:32 -0500

    Add project files.

---
+++ ScreenshotAutoUI.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ScreenshotAuto
+{
+    partial class ScreenshotAutoUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.screenshotAutoSide_Panel = new System.Windows.Forms.Panel();
+            this.screenshotAutoTop_Panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.waitB4_TB = new System.Windows.Forms.TextBox();
+            this.Import_BTN = new System.Windows.Forms.Button();
+            this.filePath_TB = new System.Windows.Forms.TextBox();
+            this.savePath_TB = new System.Windows.Forms.TextBox();
+            this.processStatus_LBL = new System.Windows.Forms.Label();
+            this.Prepare_BTN = new System.Windows.Forms.Button();
+            this.possibleSizes_DGV = new System.Windows.Forms.DataGridView();
+            this.percentOfMemory_DGV = new System.Windows.Forms.DataGridView();
+            this.SavePath_BTN = new System.Windows.Forms.Button();
+            this.screenshotAutoSide_Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.possibleSizes_DGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.percentOfMemory_DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screenshotAutoSide_Panel
+            // 
+            this.screenshotAutoSide_Panel.BackColor = System.Drawing.Color.LightSlateGray;
+            this.screenshotAutoSide_Panel.Controls.Add(this.SavePath_BTN);
+            this.screenshotAutoSide_Panel.Controls.Add(this.percentOfMemory_DGV);
+            this.screenshotAutoSide_Panel.Controls.Add(this.possibleSizes_DGV);
+            this.screenshotAutoSide_Panel.Controls.Add(this.Prepare_BTN);
+            this.screenshotAutoSide_Panel.Controls.Add(this.processStatus_LBL);
+            this.screenshotAutoSide_Panel.Controls.Add(this.savePath_TB);
+            this.screenshotAutoSide_Panel.Controls.Add(this.filePath_TB);
+            this.screenshotAutoSide_Panel.Controls.Add(this.waitB4_TB);
+            this.screenshotAutoSide_Panel.Controls.Add(this.label1);
+            this.screenshotAutoSide_Panel.Controls.Add(this.Import_BTN);
+            this.screenshotAutoSide_Panel.Location = new System.Drawing.Point(0, 92);
+            this.screenshotAutoSide_Panel.Name = "screenshotAutoSide_Panel";
+            this.screenshotAutoSide_Panel.Size = new System.Drawing.Size(320, 947);
+            this.screenshotAutoSide_Panel.TabIndex = 0;
+            // 
+            // screenshotAutoTop_Panel
+            // 
+            this.screenshotAutoTop_Panel.Location = new System.Drawing.Point(0, 0);
+            this.screenshotAutoTop_Panel.Name = "screenshotAutoTop_Panel";
+            this.screenshotAutoTop_Panel.Size = new System.Drawing.Size(1980, 92);
+            this.screenshotAutoTop_Panel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Millisecond wait before each screenshot";
+            // 
+            // waitB4_TB
+            // 
+            this.waitB4_TB.Location = new System.Drawing.Point(69, 73);
+            this.waitB4_TB.Name = "waitB4_TB";
+            this.waitB4_TB.Size = new System.Drawing.Size(100, 21);
+            this.waitB4_TB.TabIndex = 2;
+            // 
+            // Import_BTN
+            // 
+            this.Import_BTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.Import_BTN.Location = new System.Drawing.Point(80, 123);
+            this.Import_BTN.Name = "Import_BTN";
+            this.Import_BTN.Size = new System.Drawing.Size(79, 23);
+            this.Import_BTN.TabIndex = 0;
+            this.Import_BTN.Text = "Import files";
+            this.Import_BTN.UseVisualStyleBackColor = true;
+            this.Import_BTN.Click += new System.EventHandler(this.Import_BTN_Click);
+            // 
+            // filePath_TB
+            // 
+            this.filePath_TB.Location = new System.Drawing.Point(69, 177);
+            this.filePath_TB.Name = "filePath_TB";
+            this.filePath_TB.ReadOnly = true;
+            this.filePath_TB.Size = new System.Drawing.Size(100, 21);
+            this.filePath_TB.TabIndex = 3;
+            // 
+            // savePath_TB
+            // 
+            this.savePath_TB.Location = new System.Drawing.Point(69, 256);
+            this.savePath_TB.Name = "savePath_TB";
+            this.savePath_TB.ReadOnly = true;
+            this.savePath_TB.Size = new System.Drawing.Size(100, 21);
+            this.savePath_TB.TabIndex = 4;
+            // 
+            // processStatus_LBL
+            // 
+            this.processStatus_LBL.AutoSize = true;
+            this.processStatus_LBL.Location = new System.Drawing.Point(77, 717);
+            this.processStatus_LBL.Name = "processStatus_LBL";
+            this.processStatus_LBL.Size = new System.Drawing.Size(101, 16);
+            this.processStatus_LBL.TabIndex = 5;
+            this.processStatus_LBL.Text = "Process status: ";
+            // 
+            // Prepare_BTN
+            // 
+            this.Prepare_BTN.Location = new System.Drawing.Point(38, 361);
+            this.Prepare_BTN.Name = "Prepare_BTN";
+            this.Prepare_BTN.Size = new System.Drawing.Size(158, 44);
+            this.Prepare_BTN.TabIndex = 6;
+            this.Prepare_BTN.Text = "Make calculations to prepare for process";
+            this.Prepare_BTN.UseVisualStyleBackColor = true;
+            this.Prepare_BTN.Click += new System.EventHandler(this.Prepare_BTN_Click);
+            // 
+            // possibleSizes_DGV
+            // 
+            this.possibleSizes_DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.possibleSizes_DGV.Location = new System.Drawing.Point(15, 433);
+            this.possibleSizes_DGV.Name = "possibleSizes_DGV";
+            this.possibleSizes_DGV.Size = new System.Drawing.Size(102, 229);
+            this.possibleSizes_DGV.TabIndex = 7;
+            // 
+            // percentOfMemory_DGV
+            // 
+            this.percentOfMemory_DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.percentOfMemory_DGV.Location = new System.Drawing.Point(184, 433);
+            this.percentOfMemory_DGV.Name = "percentOfMemory_DGV";
+            this.percentOfMemory_DGV.Size = new System.Drawing.Size(102, 229);
+            this.percentOfMemory_DGV.TabIndex = 8;
+            // 
+            // SavePath_BTN
+            // 
+            this.SavePath_BTN.Location = new System.Drawing.Point(48, 210);
+            this.SavePath_BTN.Name = "SavePath_BTN";
+            this.SavePath_BTN.Size = new System.Drawing.Size(130, 23);
+            this.SavePath_BTN.TabIndex = 9;
+            this.SavePath_BTN.Text = "Select save path";
+            this.SavePath_BTN.UseVisualStyleBackColor = true;
+            this.SavePath_BTN.Click += new System.EventHandler(this.SavePath_BTN_Click);
+            // 
+            // ScreenshotAutoUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1980, 1040);
+            this.Controls.Add(this.screenshotAutoTop_Panel);
+            this.Controls.Add(this.screenshotAutoSide_Panel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ScreenshotAutoUI";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Silver;
+            this.Load += new System.EventHandler(this.ScreenshotAutoUI_Load);
+            this.screenshotAutoSide_Panel.ResumeLayout(false);
+            this.screenshotAutoSide_Panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.possibleSizes_DGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.percentOfMemory_DGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screenshotAutoSide_Panel;
+        private System.Windows.Forms.Panel screenshotAutoTop_Panel;
+        private System.Windows.Forms.TextBox waitB4_TB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox savePath_TB;
+        private System.Windows.Forms.TextBox filePath_TB;
+        private System.Windows.Forms.Button Import_BTN;
+        private System.Windows.Forms.Label processStatus_LBL;
+        private System.Windows.Forms.Button Prepare_BTN;
+        private System.Windows.Forms.DataGridView possibleSizes_DGV;
+        private System.Windows.Forms.DataGridView percentOfMemory_DGV;
+        private System.Windows.Forms.Button SavePath_BTN;
+    }
+}
+
---
+++ ScreenshotAutoUI.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Imaging;
+using System.Runtime.Remoting.Messaging;
+using System.Linq.Expressions;
+using System.Windows.Automation;
+using System.Runtime.InteropServices;
+using Open.Numeric.Primes;
+using System.Numerics;
+using Open.Numeric.Primes.Extensions;
+using System.Globalization;
+using Open.Collections;
+namespace ScreenshotAuto
+{
+    public partial class ScreenshotAutoUI : Form
+    {
+        Process screenshotAuto;
+        private int totalScreenshots;
+        private Size screenshotSize;
+        private int waitB4;
+        private string savePath;
+        private int completedScreenshots;
+        private readonly List<string> fileNames = new List<string>();
+        private readonly List<string> filePaths = new List<string>();
+
+
+        public ScreenshotAutoUI()
+        {
+            InitializeComponent();
+        }
+
+        private void ScreenshotAutoUI_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Size formSize = new System.Drawing.Size(Screen.GetWorkingArea(this).Width, Screen.GetWorkingArea(this).Height);
+            this.Size = formSize;
+            this.Location = new System.Drawing.Point(0, 0);
+            this.TopMost = true;
+        }
+
+        private void Import_BTN_Click(object sender, EventArgs e)
+        {
+            processStatus_LBL.Text = "Waiting to process files...";
+            try
+            {
+                using (FolderBrowserDialog dialogBD = new FolderBrowserDialog())
+                {
+                    dialogBD.Description = "Please select the folder that has the files you want to be screenshotted.";
+                    dialogBD.ShowNewFolderButton = false;
+                    dialogBD.ShowDialog();
+                    filePath_TB.Text = dialogBD.SelectedPath;
+
+                }
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show($"Data: {f.Data}\n HResult: + {f.HResult} \n helplink: {f.HelpLink}\n + InnerException: " +
+                  $"{f.InnerException}\n + Message: {f.Message}\n +Source: {f.Source}\n + StackTrace: {f.StackTrace}\n + " +
+                  $"TargetSite: {f.TargetSite}", "Error", MessageBoxButtons.OK);
+            }
+            finally
+            {
+                DirectoryInfo fileInfo = new DirectoryInfo(filePath_TB.Text);
+
+                //Make sure all the files in the folder they selected for filePath are nif files, because that is the only file type nifskope can open. 
+
+                foreach (var file in fileInfo.GetFiles())
+                {
+                    if (file.Extension.Equals(".nif"))
+                    {
+                        fileNames.Add(file.Name);
+                        filePaths.Add(file.FullName);
+                    }
+                }
+               
+                int waitB4 = int.Parse(waitB4_TB.Text);
+            }
+            processStatus_LBL.Text = "Files processed!";
+        }
+        private void SavePath_BTN_Click(object sender, EventArgs e)
+        {
+           using(FolderBrowserDialog saveDB = new FolderBrowserDialog())
+            {
+                saveDB.Description = "Please select a folder that you want the screenshots to be saved to. Though not required, I would recommend that you pick an empty one. But it does need to be not the same folder that contains the files.";
+                saveDB.ShowNewFolderButton = true;
+                saveDB.ShowDialog();
+                savePath_TB.Text = saveDB.SelectedPath;
+                savePath = savePath_TB.Text;
+            }
+        }
+        private void GetTotalNumberOfFiles()
+        {
+            string filePath = filePath_TB.Text;
+            DirectoryInfo files = new DirectoryInfo(filePath);
+            int totalScreenshots = files.GetFiles().Count();
+        }
+
+
+        private void Prepare_BTN_Click(object sender, EventArgs e)
+        {
+            long availableSpaceInBytes;
+            double totalMemoryForScreenshots;
+            List<BigInteger> factors = new List<BigInteger>();
+            int bytesPerPixel;
+            GetTotalNumberOfFiles();
+            using (Process prepProcess = new Process())
+            {
+                IntPtr OpenNifskope()
+                {
+                    prepProcess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+                    prepProcess.StartInfo.FileName = filePaths[0];
+                    prepProcess.Start();
+                    while (prepProcess.MainWindowHandle == IntPtr.Zero)
+                    {
+                        prepProcess.WaitForInputIdle();
+                    }
+                    AutomationElement elementThatWillNotBeEnabledUntilNifLoads = AutomationElement.FromHandle(prepProcess.MainWindowHandle).FindFirst(TreeScope.Subtree, new PropertyCondition(AutomationElement.NameProperty, "Save View"));
+                    while (elementThatWillNotBeEnabledUntilNifLoads.Current.IsEnabled == false)
+                    {
+                        prepProcess.WaitForInputIdle();
+                    }
+                    IntPtr handleToReturn = prepProcess.MainWindowHandle;
+                    return handleToReturn;
+                }
+                IntPtr nifskopeMainWindowHandle = OpenNifskope();
+                System.Windows.Rect GetViewportRect()
+                {
+                    AutomationElement viewportElement = AutomationElement.FromHandle(nifskopeMainWindowHandle).FindFirst(TreeScope.Subtree, new PropertyCondition(AutomationElement.ClassNameProperty, "Qt5QWindowOwnDCIcon"));
+                    System.Windows.Rect rectToReturn = viewportElement.Current.BoundingRectangle;
+                    return rectToReturn;
+                }
+                System.Windows.Rect viewportRect = GetViewportRect();
+                List<Size> DeterminePossibleSizes()
+                {
+                    List<Size> possibleSizes = new List<Size>();
+                    int i = 0;
+                    double width = viewportRect.Width;
+                    double height = viewportRect.Height;
+                    List<Size> sizeCandidatesToReturn = new List<Size>();
+                    DriveInfo saveDrive = new DriveInfo(savePath);
+                    availableSpaceInBytes = saveDrive.AvailableFreeSpace;
+                    bytesPerPixel = Screen.FromHandle(prepProcess.MainWindowHandle).BitsPerPixel / 8;
+                    totalMemoryForScreenshots = viewportRect.Width * viewportRect.Height * bytesPerPixel * totalScreenshots;
+                    bool thereWillBeEnoughSpaceOnDriveIfSceenshotSizeEqualsViewportSize = Math.Max(totalMemoryForScreenshots, availableSpaceInBytes).Equals(availableSpaceInBytes);
+                    factors.AddRange(Prime.CommonFactors((int)viewportRect.Width, (int)viewportRect.Height));
+                    Size possibleSize = new Size();
+                    switch (thereWillBeEnoughSpaceOnDriveIfSceenshotSizeEqualsViewportSize)
+                    {
+                       
+                        case true:
+                            while (totalMemoryForScreenshots < availableSpaceInBytes);
+                            {
+                                i++;
+                                width *= i;
+                                height *= i;
+                                totalMemoryForScreenshots = width * height * bytesPerPixel * totalScreenshots;
+                                possibleSize = new Size((int)width, (int)height);
+                                sizeCandidatesToReturn.Add(possibleSize);
+                            }
+                            i = 0;
+                            foreach (BigInteger factor in factors)
+                            {
+                               
+                                width = width / (double)factors[i];
+                                height = height / (double)factors[i];
+                                possibleSize = new Size((int)width, (int)height);
+                                sizeCandidatesToReturn.Add(possibleSize);
+                                i++;
+                            }
+                            break;
+                        case false:
+                            i = 0;
+                            foreach (BigInteger factor in factors)
+                            {
+                                width /= (int)factors[i];
+                                height /= (int)factors[i];
+                                if (height * width * bytesPerPixel * totalScreenshots < availableSpaceInBytes)
+                                {
+                                    possibleSize = new Size((int)width, (int)height);
+                                    sizeCandidatesToReturn.Add(possibleSize);
+                                }
+                            }
+                            break;
+                    }
+                    return sizeCandidatesToReturn;
+
+                }
+                List<Size> sizeCandidates = DeterminePossibleSizes();
+                List<string> CalculatePercentOfMemoryScreenshotsWillTakeUpAtEachSize()
+                {
+                    int i = 0;
+                    List<string> percentagesToReturn = new List<string>();  
+                    foreach(Size size in sizeCandidates)
+                    {
+                        totalMemoryForScreenshots = sizeCandidates[i].Width * sizeCandidates[i].Height * bytesPerPixel * totalScreenshots;
+                        decimal percent = (((decimal)totalMemoryForScreenshots) / availableSpaceInBytes) * 100;
+                        string percentString = string.Join("", percent, "%");
+                        percentagesToReturn.Add(percentString);
+                        i++;
+                    }
+                    return percentagesToReturn;
+
+                }
+                List<string> percentages = CalculatePercentOfMemoryScreenshotsWillTakeUpAtEachSize();
+                void FillOutDataGridViews()
+                {
+                    possibleSizes_DGV.DataSource = sizeCandidates;
+                    percentOfMemory_DGV.DataSource = percentages;
+                }
+                FillOutDataGridViews();
+                MessageBox.Show("On the ui of ScreenshotAuto, you should  see two data grid views. The one on the left tells you the possible sizes each screenshot could be while respecting the aspect ratio of the viewport, and the one on the left tells you the percentage of the total available space the screenshots will take up. When you are ready to start the process, go ahead and select one of the percentages, then click start.");
+            }
+
+        }
+
+       
+    }
+}
+
